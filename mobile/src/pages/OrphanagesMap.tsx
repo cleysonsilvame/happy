7bba6616a79de2b34c75cbea856623039597c1c8
Cleyson Silva <cleysonsilva.me@gmail.com>
2020-10-19 16:47:02 -0300

    fix workshop - day 5

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import { useNavigation } from '@react-navigation/native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
-import { RectButton } from 'react-native-gesture-handler';
-
 import mapMarker from '../images/map-marker.png';
-
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 import api from '../services/api';
 
 interface Orphanage {
@@ -16,15 +14,15 @@ interface Orphanage {
   longitude: number;
 }
 
-export default function OrphanagesMap() {
-  const navigation = useNavigation();
+export default function OrphangesMap() {
   const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+  const navigation = useNavigation();
 
-  useEffect(() => {
+  useFocusEffect(() => {
     api.get('orphanages').then((response) => {
       setOrphanages(response.data);
     });
-  }, []);
+  });
 
   function handleNavigateToOrphanageDetails(id: number) {
     navigation.navigate('OrphanageDetails', { id });
@@ -40,10 +38,10 @@ export default function OrphanagesMap() {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
-          latitude: -23.454563,
-          longitude: -46.534393,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
+          latitude: -23.4528386,
+          longitude: -46.5182909,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
         }}
       >
         {orphanages.map((orphanage) => {
@@ -51,7 +49,10 @@ export default function OrphanagesMap() {
             <Marker
               key={orphanage.id}
               icon={mapMarker}
-              calloutAnchor={{ x: 2.7, y: 0.8 }}
+              calloutAnchor={{
+                x: 2.7,
+                y: 0.8,
+              }}
               coordinate={{
                 latitude: orphanage.latitude,
                 longitude: orphanage.longitude,
@@ -69,12 +70,11 @@ export default function OrphanagesMap() {
           );
         })}
       </MapView>
-
       <View style={styles.footer}>
         <Text style={styles.footerText}>
-          {orphanages.length} orfanatos encontrados
+          {' '}
+          {orphanages.length} orfanatos encontrados{' '}
         </Text>
-
         <RectButton
           style={styles.createOrphanageButton}
           onPress={handleNavigateToCreateOrphanage}
@@ -100,15 +100,17 @@ const styles = StyleSheet.create({
     width: 160,
     height: 46,
     paddingHorizontal: 16,
-    backgroundColor: 'rgba(255,255,255,0.8)',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
     borderRadius: 16,
     justifyContent: 'center',
   },
+
   calloutText: {
     color: '#0089a5',
     fontSize: 14,
     fontFamily: 'Nunito_700Bold',
   },
+
   footer: {
     position: 'absolute',
     left: 24,
@@ -124,12 +126,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
 
-    elevation: 3,
+    elevation: 3, //Sombra
   },
+
   footerText: {
-    color: '#8fa7b3',
     fontFamily: 'Nunito_700Bold',
+    color: '#8fa7b3',
   },
+
   createOrphanageButton: {
     width: 56,
     height: 56,
